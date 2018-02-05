c69098c53ba9d420b7ecb0ccd431d2a5deae9c86
zhu-ty <zhu-ty@outlook.com>
2018-02-05 21:10:42 +0800

    first

---
+++ drawing/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace drawing
+{
+    partial class FormDrawing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.GenerateB = new System.Windows.Forms.Button();
+            this.SaveB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1171, 648);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GenerateB
+            // 
+            this.GenerateB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.GenerateB.Location = new System.Drawing.Point(1027, 666);
+            this.GenerateB.Name = "GenerateB";
+            this.GenerateB.Size = new System.Drawing.Size(75, 23);
+            this.GenerateB.TabIndex = 1;
+            this.GenerateB.Text = "Generate";
+            this.GenerateB.UseVisualStyleBackColor = true;
+            this.GenerateB.Click += new System.EventHandler(this.GenerateB_Click);
+            // 
+            // SaveB
+            // 
+            this.SaveB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveB.Location = new System.Drawing.Point(1108, 666);
+            this.SaveB.Name = "SaveB";
+            this.SaveB.Size = new System.Drawing.Size(75, 23);
+            this.SaveB.TabIndex = 2;
+            this.SaveB.Text = "Save";
+            this.SaveB.UseVisualStyleBackColor = true;
+            this.SaveB.Click += new System.EventHandler(this.SaveB_Click);
+            // 
+            // FormDrawing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 699);
+            this.Controls.Add(this.SaveB);
+            this.Controls.Add(this.GenerateB);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormDrawing";
+            this.Text = "Drawing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button GenerateB;
+        private System.Windows.Forms.Button SaveB;
+    }
+}
+
---
+++ drawing/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drawing
+{
+
+
+    public partial class FormDrawing : Form
+    {
+        public FormDrawing()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(1, 1);
+        }
+
+        private void GenerateB_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            var G = Graphics.FromImage(img);
+            int size = Math.Min(pictureBox1.Width, pictureBox1.Height);
+            G.Clear(Color.White);
+
+            //Draw All
+
+            double multiple = size * 0.8 / val.all_diameter;
+            val.multiple = multiple;
+
+            G.DrawEllipse(new Pen(Color.Black, 4), val.GetRec(size / 2, size / 2, size * 0.8));
+            G.DrawLine(new Pen(Color.Gray, 1.5f), size / 2, size / 2,
+                size / 2 + (float)val.all_diameter * (float)multiple / 2 * (float)Math.Cos(0.75f * val.pi),
+                size / 2 - (float)val.all_diameter * (float)multiple / 2 * (float)Math.Sin(0.75f * val.pi));
+            G.DrawString("r = " + Math.Round((val.all_diameter / 2), 2).ToString(),
+                new Font("宋体", 10), new SolidBrush(Color.Black),
+                size / 2 + (float)val.all_diameter * (float)multiple / 2 * (float)Math.Cos(0.75f * val.pi) * 0.75f,
+                (float)(size / 2 - (float)val.all_diameter * (float)multiple / 2 * (float)Math.Sin(0.75f * val.pi) * 0.75f - size * 0.01));
+
+            //Draw bottom
+
+            G.DrawEllipse(new Pen(Color.FromArgb(255, 192, 0), 3), val.GetRec(size / 2, size / 2 - val.bottom_radius, val.normal_hole_diameter));
+            G.DrawEllipse(new Pen(Color.FromArgb(255, 192, 0), 3), val.GetRec(size / 2, size / 2 + val.bottom_radius, val.normal_hole_diameter));
+
+            G.DrawLine(new Pen(Color.Gray, 1.5f), size / 2, size / 2, size / 2, (float)(size / 2 - val.bottom_radius));
+            G.DrawString("r = " + Math.Round((val.bottom_radius / multiple), 2).ToString(),
+                new Font("宋体", 10), new SolidBrush(Color.Black),
+                size / 2, (float)(size / 2 - val.bottom_radius / 2 - size * 0.007));
+
+            //Draw connect
+
+            for (int i = -1; i <= 1; i += 2)
+                for (int j = -1; j <= 1; j += 2)
+                {
+                    G.DrawEllipse(new Pen(Color.Red, 3), val.GetRec(size / 2 + i * val.connect_radius, size / 2 + j * val.connect_radius, val.normal_hole_diameter));
+                }
+
+            G.DrawLine(new Pen(Color.Gray, 1.5f), size / 2, size / 2, size / 2 + 1 * (float)val.connect_radius, size / 2 + 1 * (float)val.connect_radius);
+            G.DrawString("r = " + Math.Round((val.connect_radius / multiple), 2).ToString(),
+                new Font("宋体", 10), new SolidBrush(Color.Black),
+                size / 2 + 0.5f * (float)val.connect_radius, (float)(size / 2 + 0.5f * (float)val.connect_radius - size * 0.01));
+
+            //Draw wire
+
+            for (int i = 0; i < 4; i++)
+            {
+                G.DrawEllipse(new Pen(Color.Green, 3),
+                    val.GetRec(size / 2 + val.wire_radius * Math.Cos(val.pi / 2 * i),
+                    size / 2 + val.wire_radius * Math.Sin(val.pi / 2 * i),
+                    val.wire_hole_diameter));
+            }
+
+            G.DrawLine(new Pen(Color.Gray, 1.5f), size / 2, size / 2, size / 2 + (float)val.wire_radius, size / 2);
+            G.DrawString("r = " + Math.Round((val.wire_radius / multiple), 2).ToString(),
+                new Font("宋体", 10), new SolidBrush(Color.Black),
+                (float)(size / 2 + 0.5f * val.wire_radius), (float)(size / 2 - size * 0.02));
+
+            //Draw Camera
+
+            for (int i = 0; i < val.hole_count; i++)
+            {
+                G.DrawEllipse(new Pen(Color.Blue, 3),
+                    val.GetRec(size / 2 + val.camera_radius * Math.Cos(val.pi * 2 / val.hole_count * i),
+                    size / 2 + val.camera_radius * Math.Sin(val.pi * 2 / val.hole_count * i),
+                    val.normal_hole_diameter));
+            }
+
+            G.DrawLine(new Pen(Color.Gray, 1.5f), size / 2, size / 2,
+                (float)(size / 2 + val.camera_radius * Math.Cos(val.pi * 2 / val.hole_count * 1)),
+                (float)(size / 2 + val.camera_radius * Math.Sin(val.pi * 2 / val.hole_count * 1)));
+            G.DrawString("r = " + Math.Round((val.camera_radius / multiple), 2).ToString(),
+                new Font("宋体", 10), new SolidBrush(Color.Black),
+                (float)(size / 2 + 0.5f * val.camera_radius * Math.Cos(val.pi * 2 / val.hole_count * 1)),
+                (float)(size / 2 + 0.5f * val.camera_radius * Math.Sin(val.pi * 2 / val.hole_count * 1) - size * 0.01));
+
+
+            //2 extra dia
+            //wire
+            G.DrawLine(new Pen(Color.Gray, 1.5f),
+                (float)(size / 2 - val.wire_radius - val.wire_hole_diameter / 2),
+                size / 2,
+                (float)(size / 2 - val.wire_radius + val.wire_hole_diameter / 2),
+                size / 2);
+            G.DrawString("d = " + Math.Round((val.wire_hole_diameter / multiple), 2).ToString(),
+                new Font("宋体", 10), new SolidBrush(Color.Black),
+                (float)(size / 2 - val.wire_radius - size * 0.02),
+                (float)(size / 2 - size * 0.02));
+
+            //normal_hole
+            G.DrawLine(new Pen(Color.Gray, 1.5f),
+                (float)(size / 2 + val.camera_radius * Math.Cos(val.pi * 2 / val.hole_count * 2) - val.normal_hole_diameter / 2),
+                (float)(size / 2 + val.camera_radius * Math.Sin(val.pi * 2 / val.hole_count * 2)),
+                (float)(size / 2 + val.camera_radius * Math.Cos(val.pi * 2 / val.hole_count * 2) + val.normal_hole_diameter / 2),
+                (float)(size / 2 + val.camera_radius * Math.Sin(val.pi * 2 / val.hole_count * 2)));
+            G.DrawString("d = " + Math.Round((val.normal_hole_diameter / multiple), 2).ToString(),
+                new Font("宋体", 10), new SolidBrush(Color.Black),
+                (float)(size / 2 + val.camera_radius * Math.Cos(val.pi * 2 / val.hole_count * 2) - size * 0.02),
+                (float)(size / 2 + val.camera_radius * Math.Sin(val.pi * 2 / val.hole_count * 2) - size * 0.03));
+
+
+            pictureBox1.Image = img;
+        }
+
+        private void SaveB_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save(DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString().Replace(':', '-') + ".bmp");
+        }
+    }
+
+    public class val
+    {
+        public static double pi = Math.PI;
+        public static double multiple = 1.0;
+
+        public static double all_diameter = 280;
+        public static double normal_hole_diameter
+        {
+            get
+            {
+                return 6.35 * multiple;
+            }
+        }
+        public static double wire_hole_diameter
+        {
+            get
+            {
+                return 40 * multiple;
+            }
+        }
+
+        public static double connect_radius
+        {
+            get
+            {
+                return 70 * Math.Sqrt(2) / 2 * multiple;
+            }
+        }
+        public static double wire_radius
+        {
+            get
+            {
+                return 70 * multiple;
+            }
+        }
+        public static double bottom_radius
+        {
+            get
+            {
+                return 10 * multiple;
+            }
+        }
+        public static double camera_radius
+        {
+            get
+            {
+                return 130 * multiple;
+            }
+        }
+
+        public const int hole_count = 5;
+
+        public static RectangleF GetRec(double cx, double cy, double d)
+        {
+            return new RectangleF((float)(cx - d / 2), (float)(cy - d / 2), (float)d, (float)d);
+        }
+    }
+}
